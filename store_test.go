2bb9bd42fdca15e309871d759b11caeaf91b8c34
Brian Kassouf <brian.m.kassouf@gmail.com>
2013-08-09 16:13:04 -0700

    change old storage tests to be Memory Storage tests only

--- store_test.go
+++ store_test.go
@@ -2,64 +2,70 @@ package main
 
 import "testing"
 
+var MemStore = MemoryStore{make(map[string]*Socket), 0}
+
+func makeSock(UID string) *Socket {
+    return &Socket{nil, UID, make(chan *Message), make(chan bool), nil}
+}
+
 func TestSave(t *testing.T) {
-    Store.Save("TEST", &Socket{nil, "TEST", make(chan *Message), make(chan bool)})
+    MemStore.Save("TEST", makeSock("TEST"))
     
-    _, exists := Store.clients["TEST"]
+    _, exists := MemStore.clients["TEST"]
     if(!exists) {
         t.Errorf("Save Test failed, Client not found")
     }
     
-    if(Store.clientCount != 1) {
-        t.Errorf("Save Test failed, clientCount = %v, want %v", Store.clientCount, 1)
+    if(MemStore.clientCount != 1) {
+        t.Errorf("Save Test failed, clientCount = %v, want %v", MemStore.clientCount, 1)
     }
     
-    Store.Save("TEST1", &Socket{nil, "TEST1", make(chan *Message), make(chan bool)})
-    if(Store.clientCount != 2) {
-        t.Errorf("Save Test failed, clientCount = %v, want %v", Store.clientCount, 2)
+    MemStore.Save("TEST1", makeSock("TEST1"))
+    if(MemStore.clientCount != 2) {
+        t.Errorf("Save Test failed, clientCount = %v, want %v", MemStore.clientCount, 2)
     }
     
-    Store.Save("TEST1", &Socket{nil, "TEST1", make(chan *Message), make(chan bool)})
-    if(Store.clientCount != 2) {
-        t.Errorf("Save Test failed, clientCount = %v, want %v", Store.clientCount, 2)
+    MemStore.Save("TEST1", makeSock("TEST1"))
+    if(MemStore.clientCount != 2) {
+        t.Errorf("Save Test failed, clientCount = %v, want %v", MemStore.clientCount, 2)
     }
 }
 
 func TestRemove(t *testing.T) {
-    if(Store.clientCount != 2) {
-        t.Errorf("Remove Test is invalid, clientCount = %v, want %v", Store.clientCount, 2)
+    if(MemStore.clientCount != 2) {
+        t.Errorf("Remove Test is invalid, clientCount = %v, want %v", MemStore.clientCount, 2)
     }
     
-    Store.Remove("TEST")
-    _, exists := Store.clients["TEST"];
+    MemStore.Remove("TEST")
+    _, exists := MemStore.clients["TEST"];
     
     if exists {
         t.Errorf("Remove Test failed, Client was not removed")
     }
     
-    if(Store.clientCount != 1) {
-        t.Errorf("Remove Test failed, clientCount = %v, want %v", Store.clientCount, 1)
+    if(MemStore.clientCount != 1) {
+        t.Errorf("Remove Test failed, clientCount = %v, want %v", MemStore.clientCount, 1)
     }
     
-    Store.Remove("TEST")
-    if(Store.clientCount != 1) {
-        t.Errorf("Remove Test failed, clientCount = %v, want %v", Store.clientCount, 1)
+    MemStore.Remove("TEST")
+    if(MemStore.clientCount != 1) {
+        t.Errorf("Remove Test failed, clientCount = %v, want %v", MemStore.clientCount, 1)
     }
     
-    Store.Remove("TEST1")
-    if(Store.clientCount != 0) {
-        t.Errorf("Remove Test failed, clientCount = %v, want %v", Store.clientCount, 0)
+    MemStore.Remove("TEST1")
+    if(MemStore.clientCount != 0) {
+        t.Errorf("Remove Test failed, clientCount = %v, want %v", MemStore.clientCount, 0)
     }
     
-    if len(Store.clients) != 0 {
+    if len(MemStore.clients) != 0 {
         t.Errorf("Remove Test failed, clients map expected to be empty")
     }
 }
 
 func TestClient(t *testing.T) {
-    Store.Save("TEST1", &Socket{nil, "TEST1", make(chan *Message), make(chan bool)})
+    MemStore.Save("TEST1", makeSock("TEST1"))
     
-    client, err := Store.Client("TEST1");
+    client, err := MemStore.Client("TEST1");
     if err != nil {
         t.Errorf("Client Test failed, client TEST1 should exist")
     }
@@ -70,23 +76,23 @@ func TestClient(t *testing.T) {
         t.Errorf("Client Test failed, could not access client TEST1's data")
     }
 
-    _, err1 := Store.Client("N/A");
+    _, err1 := MemStore.Client("N/A");
     if err1 == nil {
         t.Errorf("GetClient Test failed, non-existant client failed to throw error")
     }
-    Store.Remove("TEST1")
+    MemStore.Remove("TEST1")
 }
 
 func TestGetCount(t *testing.T) {
-    Store.Save("TEST3", &Socket{nil, "TEST1", make(chan *Message), make(chan bool)})
+    MemStore.Save("TEST3", makeSock("TEST3"))
 
-    count, _ := Store.Count()
+    count, _ := MemStore.Count()
     if count != 1 {
         t.Errorf("GetCount Test failed. ClientCount = %v, expected %v", count, 1)
     }
 
-    Store.Save("TEST4", &Socket{nil, "TEST1", make(chan *Message), make(chan bool)})
-    count, _ = Store.Count()
+    MemStore.Save("TEST4", makeSock("TEST4"))
+    count, _ = MemStore.Count()
     if count != 2 {
         t.Errorf("GetCount Test failed. ClientCount = %v, expected %v", count, 2 )
     }
