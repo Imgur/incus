0b0a2cd8148c6c5b55085247cfecf3ebbc7cbd4e
Brian Kassouf <brian.m.kassouf@gmail.com>
2013-08-14 18:55:10 -0700

    added config file parsing functionality

--- config.go
+++ config.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+    "log"
+    "fmt"
 
+    "github.com/briankassouf/cfg"
 )
 
 type Configuration struct {
-    conf string
+    vars map[string]string
 }
 
 func initConfig() Configuration {
-    return Configuration{"TEST"}
+    mymap := make(map[string]string)
+    err := cfg.Load("sockets.conf", mymap)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return Configuration{mymap}    
+}
+
+func (this *Configuration) Get(name string) (string, error) {
+    val, ok := this.vars[name]
+    if !ok {
+        return "", fmt.Errorf("Config Error: variable '%s' not found", name)
+    }
+ 
+    return val, nil;
 }
