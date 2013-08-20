090a8ad48b1c4852b5f8779c7574b1a0693345bb
briankassouf <brian@imgur.com>
2013-08-20 22:13:03 +0000

    install script was using an old file name for the configy

--- install/install.sh
+++ install/install.sh
@@ -4,7 +4,7 @@ cp initd.sh /etc/init.d/incus
 if [ ! -d "/etc/incus" ]; then
     mkdir /etc/incus
 fi
-cp default.conf /etc/incus/incus.conf
+cp incus.conf /etc/incus/incus.conf
 cp incus /usr/sbin/incus
 
 touch /var/log/incus.log
