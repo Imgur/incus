925eeaea02f6c5620e22ca1052957d6683e9041a
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2015-07-29 12:52:33 -0700

    fix: Only deploy builds merged into master

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,8 @@
 
 VERSION=$(date +%s)
 
-aws s3 cp $GOPATH/incus.tar s3://imgur-incus/incus-latest.tar && \
-    aws s3 cp $GOPATH/incus.tar s3://imgur-incus/incus-$VERSION.tar && \
-    aws deploy create-deployment --application-name Incus --deployment-group-name Production --s3-location bundleType=tar,bucket=imgur-incus,key=incus-$VERSION.tar
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+    aws s3 cp $GOPATH/incus.tar s3://imgur-incus/incus-latest.tar && \
+        aws s3 cp $GOPATH/incus.tar s3://imgur-incus/incus-$VERSION.tar && \
+        aws deploy create-deployment --application-name Incus --deployment-group-name Production --s3-location bundleType=tar,bucket=imgur-incus,key=incus-$VERSION.tar
+fi
