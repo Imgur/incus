21d648aa60d5d366c1d2ca9e6c2fc42bb5e4f522
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2015-07-28 19:37:16 -0700

    fix: Fix syntax error in build script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ VERSION=$(date +%s)
 
 aws s3 cp $GOPATH/incus.tar s3://imgur-incus/incus-latest.tar && \
     aws s3 cp $GOPATH/incus.tar s3://imgur-incus/incus-$VERSION.tar && \
-    aws deploy create-deployment --application-name Incus --deployment-group-name Production --s3-location bundleType=tar,bucket=imgur-incus,key=incus-$VERSION.tar && \
+    aws deploy create-deployment --application-name Incus --deployment-group-name Production --s3-location bundleType=tar,bucket=imgur-incus,key=incus-$VERSION.tar
