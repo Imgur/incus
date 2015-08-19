a436c3301f1306ef23b6450d08d08f6344b3f947
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2015-08-19 19:55:46 +0000

    Disable marking as inactive when you disconnect from ws

--- sockets.go
+++ sockets.go
@@ -82,7 +82,7 @@ func (this *Socket) Close() error {
 			this.Page = ""
 		}
 
-		this.Server.Store.redis.MarkInactive(this.UID, this.SID)
+		// this.Server.Store.redis.MarkInactive(this.UID, this.SID)
 
 		this.Server.Store.Remove(this)
 		close(this.done)
