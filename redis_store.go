3561be2db70f6f656ce205f07d6c3109b175ae1b
Carlos Espinoza <carlos@imgur.com>
2015-11-24 23:00:43 +0000

    log blpops from redis

--- redis_store.go
+++ redis_store.go
@@ -181,6 +181,7 @@ func (this *RedisStore) Poll(c chan []byte, queue string) error {
 
 			message, err := redis.Bytes(consumer.Do("BLPOP", queue, this.pollingFreq))
 			this.CloseConn(consumer)
+            log.Printf("Message from redis: %s\n", message)
 
 			if err == nil && len(message) > 0 {
 				c <- message
